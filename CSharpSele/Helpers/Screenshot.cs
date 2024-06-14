b65f12d47ea5947b9c511031f1f87ee9710e1492
Rohit B <robinfrancis40@gmail.com>
2024-06-14 16:45:13 -0400

    Screenshots helper added

--- CSharpSele/Helpers/Screenshot.cs
+++ CSharpSele/Helpers/Screenshot.cs
@@ -1,7 +1,4 @@
 ﻿using OpenQA.Selenium;
-using OpenQA.Selenium.Support.Extensions;
-using System;
-using System.IO;
 
 public static class ScreenshotHelper
 {
@@ -11,28 +8,8 @@ public static class ScreenshotHelper
         {
             Screenshot screenshot = ((ITakesScreenshot)driver).GetScreenshot();
             string screenshotsDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Screenshots");
-
-            // Debug statement to check the path
-            Console.WriteLine($"Screenshots directory: {screenshotsDirectory}");
-
-            // Create directory if it doesn't exist
             Directory.CreateDirectory(screenshotsDirectory);
-
-            // Debug statement to check if the directory exists
-            if (Directory.Exists(screenshotsDirectory))
-            {
-                Console.WriteLine("Screenshots directory created successfully.");
-            }
-            else
-            {
-                Console.WriteLine("Failed to create screenshots directory.");
-            }
-
             string filePath = Path.Combine(screenshotsDirectory, $"{testName}_{DateTime.Now:yyyyMMdd_HHmmss}.png");
-
-            // Debug statement to check the file path
-            Console.WriteLine($"Screenshot file path: {filePath}");
-
             screenshot.SaveAsFile(filePath);
         }
         catch (Exception ex)
